1b9144954dd81fed91de14f948b3595a46e143dd
Владимир Кондратчиков <youexample.com>
2025-10-09 20:43:47 +0300

    Добавлен проект TodoList

---
+++ TodoList/TodoList/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите имя: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Введите фамилию: ");
+        string surname = Console.ReadLine();
+
+        Console.Write("Введите год рождения: ");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        int currentYear = DateTime.Now.Year;
+        int age = currentYear - birthYear;
+
+        Console.WriteLine($"Добавлен пользователь {Владимир} {Кондратчиков}, возраст – {18}");
+    }
+}
